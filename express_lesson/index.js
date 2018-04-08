d8509d5435209239d321f3927d96984f99d4c0ee
香饽饽 <1377699408@qq.com>
2018-04-08 10:28:45 +0800

    express

--- express_lesson/index.js
+++ express_lesson/index.js
@@ -8,12 +8,14 @@ const cources = [
 ]
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World!!!!");
 })
 app.get("/api/cources/", (req, res) => {
+    console.log("查询所有课程");
     res.send(cources);
 })
 app.get("/api/cources/:id", (req, res) => {
+    console.log("查询单个课程");
     const cource = cources.find(c => c.id === parseInt(req.params.id));
     if (!cource) res.status(404).send("The cource Id you have given has not found");
     res.send(cource);
